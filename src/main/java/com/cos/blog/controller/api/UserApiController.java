bd84c86a1bb893b4c7cc6c91ea1ff3d67555f1bb
yha11 <aeblys2@naver.com>
2023-02-12 23:57:58 +0900

    46강까지

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +19,29 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
-		System.out.println("UserApiController : sava 호출됨");
+		System.out.println("UserApiController : save 호출됨");
 		// 실제로 DB에 insert를 하고 아래에서 return이 되면 돼요.
 		user.setRole(RoleType.USER);
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+	}
+	
+	// 스프링 시큐리티 이용해서 로그인!!
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user) {
+		System.out.println("UserApiController : login 호출됨");
+		User principal = userService.로그인(user); // principal(접근주체)
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 
 }
