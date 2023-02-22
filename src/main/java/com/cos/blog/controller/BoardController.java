70ebc210af1508eb2adf17e1b2042bc7aabd4fc3
yha11 <aeblys2@naver.com>
2023-02-22 17:48:34 +0900

    54강 글목록보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,18 +1,23 @@
 package com.cos.blog.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping({"", "/"})
-	public String index() { // 컨트롤러에서 세션을 어떻게 찾는지?
+	public String index(Model model) { // 컨트롤러에서 세션을 어떻게 찾는지?
+		model.addAttribute("boards", boardService.글목록());
 		// /WEB-INF/views/index.jsp
-		return "index";
+		return "index"; // viewResolver 작동
 	}
 	
 	// USER 권한이 필요
