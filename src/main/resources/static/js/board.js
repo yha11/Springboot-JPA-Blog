1e67060e51f0c87a2dfda54243d8ec57d5d27452
yha11 <aeblys2@naver.com>
2023-04-04 11:47:04 +0900

    58강 글 삭제하기

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,9 @@ let index = {
 			$("#btn-save").on("click", ()=>{ //function(){}이 아니라 ()=>{}를 쓰는 이유 : this를 바인딩하기 위해서!!
 				this.save();
 			});
+			$("#btn-delete").on("click", ()=>{ //function(){}이 아니라 ()=>{}를 쓰는 이유 : this를 바인딩하기 위해서!!
+				this.deleteById();
+			});
 		},
 		
 		save: function() {
@@ -21,6 +24,23 @@ let index = {
 				
 			}).done(function(resp) {
 				alert("글쓰기가 완료되었습니다.");
+				location.href = "/";
+				
+			}).fail(function(error) {
+				alert(JSON.stringify(error));
+			});
+		},
+		
+		deleteById: function() {
+			var id = $("#id").text();
+			
+			$.ajax({
+				type: "DELETE",
+				url: "/api/board/"+id,
+				dataType: "json"
+				
+			}).done(function(resp) {
+				alert("삭제가 완료되었습니다.");
 				location.href = "/";
 				
 			}).fail(function(error) {
